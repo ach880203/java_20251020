16a0f476cf934402aaf6cfadee5d9d3e6788dcd7
AhnChanghyun <ach880203@gmail.com>
2025-11-06 15:50:18 +0900

    JAVA Study

--- java20251106_제네릭/src/ex03/Box.java
+++ java20251106_제네릭/src/ex03/Box.java
@@ -2,6 +2,12 @@ package ex03;
 
 public class Box<T extends Person> {
 
+	/*
+	 * 타입 제한 : <T extends Person>
+	 * T 전달될 수 있는 객체는
+	 * Person 이거나 Person 자손들만 가능
+	 */
+	
 	T item;
 
 	void setItem(T item) {
